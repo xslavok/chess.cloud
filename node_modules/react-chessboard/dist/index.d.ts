01821eba6566ebc93c59b1bc8a8cb3de0cc208c1
Xslavok <ehitman011@gmail.com>
2026-06-01 04:55:06 +0200

    Izbacen standard_rating_list i node_modules

--- node_modules/react-chessboard/dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-export * from './Chessboard.js';
-export * from './ChessboardProvider.js';
-export * from './defaults.js';
-export * from './pieces.js';
-export * from './SparePiece.js';
-export * from './types.js';
-export * from './utils.js';
